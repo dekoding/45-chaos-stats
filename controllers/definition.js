fdfadf212ac5842e5e0327c6c4175969cf1eea89
dekoding <gooddamon@gmail.com>
2018-12-28 01:54:39 +0000

    Fixed order for syncing and added error handling

--- controllers/definition.js
+++ controllers/definition.js
@@ -1,37 +1,43 @@
 const Definition = require('../models').Definition;
 
 module.exports = {
-  list(req, res) {
-    return Definition
-      .findAll()
-      .then(function (definitions) {
-        defDict = {}
-        for (var i = 0; i < definitions.length; i++) {
-          defDict[definitions[i].Name] = definitions[i].Definition
-        }
-        res.status(200).send(defDict)
-      })
-      .catch((error) => { res.status(400).send(error); });
-  },
+    list(req, res) {
+        return Definition
+        .findAll()
+        .then(function (definitions) {
+            defDict = {}
+            for (var i = 0; i < definitions.length; i++) {
+                defDict[definitions[i].Name] = definitions[i].Definition
+            }
+            res.status(200).send(defDict);
+        })
+        .catch((error) => { res.status(400).send(error); });
+    },
 
-  add(rec, cb) {
-    splitRec = rec.split(',')
-    return Definition
-      .create({
-        Name: splitRec[0],
-        Definition: splitRec[1],
-      })
-      .catch((error) => cb(error));
-  },
+    add(rec, cb) {
+        splitRec = rec.split(',')
+        return Definition
+        .create({
+            Name: splitRec[0],
+            Definition: splitRec[1],
+        })
+        .catch((error) => cb(error));
+    },
 
-  destroyAll() {
-    return Definition
-      .destroy(
-        {
-          where: {}
-        }
-      )
-      .then((rows_affected) => { return rows_affected; })
-      .catch((error) => { console.error(error); });
-  }
+    destroyAll() {
+        return Definition
+        .destroy(
+            {
+                where: {}
+            }
+        )
+        .then(rows_affected => {
+            console.log(Date() + ' Destroyed existing definitions: ' + rows_affected + ' records');
+            return true;
+        })
+        .catch(error => {
+            console.error(error);
+            return false;
+        });
+    }
 }
